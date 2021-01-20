158bfbcda97b3904b0a15a7dc0f172ba2b8f25d7
clairedeboer <claire.deboer@gmail.com>
2021-01-20 09:41:40 -0500

    deliverable 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-// write your code here
+const ramenMenuDiv = document.querySelector("#ramen-menu");
+
+const getRamens = () => {
+  return fetch("http://localhost:3000/ramens").then((response) =>
+    response.json()
+  );
+};
+
+const getAndRenderRamens = () => {
+  getRamens().then((ramenData) => {
+    ramenData.forEach((ramen) => renderRamenImage(ramen));
+  });
+};
+
+getAndRenderRamens()
+
+const renderRamenImage = (ramen) => {
+  const imageTag = document.createElement("img");
+  const ramenImage = ramen.image;
+  imageTag.src = ramenImage;
+  ramenMenuDiv.append(imageTag);
+};
