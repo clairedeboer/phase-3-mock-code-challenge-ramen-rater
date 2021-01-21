efddfdcf256a3a90bc89321f6451f542c6f874a6
clairedeboer <claire.deboer@gmail.com>
2021-01-20 20:45:15 -0500

    final commit with all deliverables

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const restaurantDetail = document.querySelector(".restaurant");
 const ramenRatingForm = document.querySelector("#ramen-rating");
 const formRating = document.querySelector("#rating");
 const formComment = document.querySelector("#comment");
+const newRamenForm = document.querySelector("#new-ramen");
+const deleteButton = document.querySelector("#delete-ramen");
 
 const getRamens = () => {
   return fetch("http://localhost:3000/ramens").then((response) =>
@@ -27,8 +29,10 @@ getAndRenderRamens();
 const renderRamenImage = (ramen) => {
   const imageTag = document.createElement("img");
   imageTag.dataset.id = ramen.id;
+  imageTag.className = `thumbnail-${ramen.id}`;
   const ramenImage = ramen.image;
   imageTag.src = ramenImage;
+  imageTag.alt = ramen.name;
   ramenMenuDiv.append(imageTag);
 };
 
@@ -37,6 +41,62 @@ const findAndRenderDetail = (event) => {
   const ramen = ramenJSON.find(
     (ramenObj) => String(ramenObj.id) === String(ramenId)
   );
+
+  renderRamenDetail(ramen);
+
+  ramenRatingForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    ramen.rating = event.target.rating.value;
+    ramen.comment = event.target.comment.value;
+
+    updateRatingComment(ramen).then((ramenData) => {
+      renderUpdatedRatingComment(ramenData);
+    });
+  });
+  const deleteRamen = () => {
+    fetch(`http://localhost:3000/ramens/${ramen.id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const imageTag = document.querySelector(`.thumbnail-${ramen.id}`);
+        imageTag.remove();
+        imageDetail.remove();
+        nameDetail.remove();
+        restaurantDetail.remove();
+        formRating.remove();
+        formComment.remove();
+      });
+  };
+
+  deleteButton.addEventListener("click", deleteRamen);
+};
+
+ramenMenuDiv.addEventListener("click", findAndRenderDetail);
+
+const renderUpdatedRatingComment = (ramenData) => {
+  formRating.value = ramenData.rating;
+  formComment.textContent = ramenData.comment;
+
+  ramenRatingForm.append(formRating);
+  ramenRatingForm.append(formComment);
+};
+
+const updateRatingComment = (updatedRatingComment) => {
+  return fetch(`http://localhost:3000/ramens/${updatedRatingComment.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      rating: updatedRatingComment.rating,
+      comment: updatedRatingComment.comment,
+    }),
+  }).then((response) => response.json());
+};
+
+const renderRamenDetail = (ramen) => {
   imageDetail.src = ramen.image;
   nameDetail.textContent = ramen.name;
   restaurantDetail.textContent = ramen.restaurant;
@@ -50,40 +110,50 @@ const findAndRenderDetail = (event) => {
 
   ramenRatingForm.append(formRating);
   ramenRatingForm.append(formComment);
-
-  ramenRatingForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    ramen.rating = event.target.rating.value;
-    ramen.comment = event.target.comment.value;
-
-    updateRatingComment(ramen).then((ramenData) => {
-      renderUpdatedRatingComment(ramenData);
-    });
-  });
-}; 
-
-ramenMenuDiv.addEventListener("click", findAndRenderDetail);
-
-const renderUpdatedRatingComment = (ramenData) => {
-  formRating.value = ramenData.rating;
-  formComment.textContent = ramenData.comment;
-
-  ramenRatingForm.append(formRating);
-  ramenRatingForm.append(formComment);
 };
 
+const getRamenOne = () => {
+  return fetch("http://localhost:3000/ramens/1")
+    .then((response) => response.json())
+    .then((ramenOneData) => renderRamenDetail(ramenOneData));
+};
 
+getRamenOne();
+ramenRatingForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-const updateRatingComment = (updatedRatingComment) => {
-  return fetch(`http://localhost:3000/ramens/${updatedRatingComment.id}`, {
-    method: "PATCH",
+  ramen.rating = event.target.rating.value;
+  ramen.comment = event.target.comment.value;
+
+  updateRatingComment(ramen).then((ramenData) => {
+    renderUpdatedRatingComment(ramenData);
+  });
+});
+newRamenForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const newRamen = {
+    name: event.target.name.value, 
+    restaurant: event.target.restaurant.value, 
+    image: event.target.image.value, 
+    rating: event.target.rating.value, 
+    comment: event.target.comment.value
+  }
+
+  postNewRamenForm(newRamen)
+
+  .then((newRamenData) => {
+    renderRamenImage(newRamenData); 
+    renderRamenDetail(newRamenData);
+  }); 
+});
+
+const postNewRamenForm = (newRamen) => {
+  return fetch("http://localhost:3000/ramens", {
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      rating: updatedRatingComment.rating,
-      comment: updatedRatingComment.comment,
-    }),
+    body: JSON.stringify(newRamen),
   }).then((response) => response.json());
 };
